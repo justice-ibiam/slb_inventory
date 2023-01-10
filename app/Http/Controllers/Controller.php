fca4394750df6eeefb5160fca4efb75d120376d3
justice-ibiam <justiceibiam@gmail.com>
2023-01-10 20:38:36 +0100

    created userController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,28 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function getSuccessResponse($status = 'success', $message = 'operation successful', $data = null, $data_key = 'data'){
+
+        $res = [
+            'status' => $status,
+            'message' => $message,
+            $data_key => $data
+        ];
+        if(!$data) unset($res[$data_key]);
+        if(!$message) unset($res['message']);
+
+        return response()->json($res,200);
+    }
+
+    public static function getErrorResponse($status = 'error', $message = 'There was a problem', $data=null, $data_key='data',$code=400){
+        $res = [
+            'status' => $status,
+            'message' => $message,
+            $data_key => $data
+        ];
+        if(!$data) unset($res[$data_key]);
+
+        return response()->json($res,$code);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function(){
+    Route::group((['prefix' => 'user'], function(){
+        Route::post('/signup', )
+    }));
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
+
+
